0d9fb73cd2c4c72b85f683cd5d1aef30f6d3f604
Vissle Drissle <22083670+Vissle-Drissle@users.noreply.github.com>
2021-05-08 00:28:00 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# Stalemate
+Stalemate is a script for Chatango that logs in to your accounts to prevent them from expiration, it logs in to multiple at once and shows time taken as well as what accounts it was not able to log in to if any.
+
+## Usage
+
+- Install the latest version of [Python](https://www.python.org/)
+- While installing Python, make sure "pip" is ticked in the `Optional Features`
+- Open command prompt and type `pip install requests`, if command is not recognized try restarting your computer
+- Download the ZIP folder
+- Unzip the folder when finished downloading
+- Edit `accounts.txt` with the account details you want to log using the example format
+- Double left click on `stalemate.py` to run the script
+
+## Optional
+Since you would be inputting passwords in a text file, here are suggestions to keep those passwords safe
+- Hide the file in File Explorer by selecting the folder then `View` and "Hide selected items"
+- Remove login data from `accounts.txt` when done
+- Copy and paste the login data elsewhere
+
+## Notice
+If you do not trust this script, look at the `stalemate.py` file and read the comments in triple quotations to get an idea of what it does.
---
+++ stalemate.py
@@ -0,0 +1,55 @@
+import time
+import random
+import socket
+import requests
+import concurrent.futures
+
+""" Load the `accounts.txt` file """
+try:
+  accounts = []
+  with open("accounts.txt", "r") as file:
+    for x in file.readlines():
+      password = x.strip()
+      accounts.append(password)
+except Exception as error: accounts = []
+
+incorrect, maximum = [], len(accounts)
+
+def token(target):
+    """ Separate the login data provided by a colon then log in to Chatango to retrieve the account token """
+    accounts.remove(target)
+    query = target.split(": ", 1)
+    post = requests.post("https://chatango.com/login", data={"user_id": query[0], "password": query[1], "storecookie": "on", "checkerrors": "yes"}).cookies.get("auth.chatango.com", False)
+    if post: return [query[0], post]
+    else:
+      incorrect.append(query[0])
+      return [query[0], False]
+
+def connect(target):
+    """ Log in to Chatango private messages with the token and then immediately log out """
+    query = token(target)
+    if query[1]:
+      frame = bytes(f"tlogin:{query[1]}:2:{random.randint(1, 10 ** 16)}\x00", "utf-8")
+      log = socket.socket()
+      log.connect(("c1.chatango.com", 5222))
+      log.send(frame)
+      log.close()
+      return f'[LOGIN] {query[0]}'
+    else: return f'[INCORRECT] {query[0]}'
+
+def process():
+    """ Do the `connect(target)` function 100 times simultaneously, may or may not be CPU intensive """
+    data = 100
+    if len(accounts) < data: data = len(accounts)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=data) as action:
+      for x in action.map(connect, accounts[0:data]): print(x)
+
+print("Launching...")
+run = time.time()
+
+""" While the amount of accounts in `accounts.txt` is above 0, do the `process()` function """
+while len(accounts) > 0:
+  process()
+  print(f'[STATUS] ({maximum - len(accounts)}/{maximum}) completed in {int(time.time() - run)} seconds')
+
+input(f'[FINISH] {"Failed to login to " + ", ".join(incorrect) if incorrect else ""}')
