4446e3b0bcc0d461497ee4709620e88c4ef2bdb4
Vissle Drissle <22083670+Vissle-Drissle@users.noreply.github.com>
2023-03-28 13:21:19 -0400

    Update stalemate.py

--- stalemate.py
+++ stalemate.py
@@ -2,12 +2,12 @@ import time
 import asyncio
 import websockets
 
-# stalemate.py v2.0 by Vissle Drissle
+# stalemate.py v3.0 by Vissle Drissle
 
 """ Load the `accounts.txt` file """
 try:
   accounts = []
-  with open("accounts.txt", "r") as file:
+  with open("accounts.txt", "r", encoding="latin-1") as file:
     for x in file.readlines():
       password = x.strip()
       accounts.append(password)
@@ -37,7 +37,9 @@ async def login(username, password):
           connected = False
           await client.close()
           return [False, username, False]
-  except: return [False, username, False]
+  except Exception as error:
+    print(error)
+    return [False, username, False]
 
 """ Show results of logins """
 async def launch():
@@ -48,31 +50,39 @@ async def launch():
     if x[0]: total.append(f"{x[1]}: {x[2]} unread messages")
     else:
       fail.append(x[1])
-      total.append(f"{x[1]}: ")
+      total.append(f"{x[1]}: [Incorrect password]")
   parse = "\n".join(total)
   print(f"Finished in {int(time.time() - run)} seconds.")
   if fail:
-    with open("accounts.txt", "w") as file: file.write(parse)
-    content = input(f'Logged ({len(accounts) - len(fail)}/{len(accounts)}) accounts, check "accounts.txt" for more details.\nType "clean" to only show accounts that failed to log in. (Close "accounts.txt" if open before typing)')
+    with open("stalemate.txt", "w+") as file: file.write(parse)
+    content = input(f'Logged ({len(accounts) - len(fail)}/{len(accounts)}) accounts, check "stalemate.txt" for more details.\nType "clean" to only show accounts that failed to log in. (Close "stalemate.txt" if open before typing)\n')
     if content == "clean":
       failed = "\n".join(fail)
-      with open("accounts.txt", "w") as file: file.write(failed)
+      with open("stalemate.txt", "w+") as file: file.write(failed)
     else: input()
   else:
-    with open("accounts.txt", "w") as file: file.write(parse)
-    input('All accounts were logged, check "accounts.txt" to see the amount of unread messages for each account.')
+    with open("stalemate.txt", "w+") as file: file.write(parse)
+    input('All accounts were logged, check "stalemate.txt" to see the amount of unread messages for each account.')
 
 """ Login data separated by a colon (username: password) """
 if len(accounts) > 0:
+  fail = []
   for x in accounts:
     data = x.split(": ")
-    username = data[0]
-    password = data[1]
-    function = login(username, password)
-    detail.append(function)
+    if len(data) > 1:
+      username = data[0]
+      password = data[1]
+      function = login(username, password)
+      detail.append(function)
+    else:
+      fail.append(x)
+      break
 
-  print("Launching...")
-  run = time.time()
-  script = launch()
-  asyncio.run(script)
+  if fail:
+    print(f'Incorrect format was used, please check the line below in accounts.txt\n{fail}\nThe format is "username: password"')
+  else:
+    print("Launching...")
+    run = time.time()
+    script = launch()
+    asyncio.run(script)
 else: print('"accounts.txt" is empty.')
